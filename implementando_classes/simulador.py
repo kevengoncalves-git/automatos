6d1846347484b4968254624833f7b207b9f675d3
Keven <kevenkaue25@gmail.com>
2026-01-31 15:48:32 -0300

    voltando a planos iniciais

--- implementando_classes/simulador.py
+++
@@ -1,9 +0,0 @@
-from automato_finito import Automato
-
-print("-"*50)
-print("Bem vindo ao conversor e validador de autômatos")
-print("-"*50)
-
-automato = Automato()
-
-automato.definir_automato()
